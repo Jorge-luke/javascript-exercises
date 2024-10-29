c244afcd126b5574ff548693dc7a6f06e30154cd
jorge-luke <jorgestl123@gmail.com>
2024-10-29 08:35:18 -0300

    finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+let y = 400;
 
+const leapYears = function(year) {
+// divisible by 4, not divisible by 100, divisible by 400
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+    return true;
+    } else {
+        return false;
+    }
 };
+
+
+let question = leapYears(y);
+
+console.log(question);
+
 
 // Do not edit below this line
 module.exports = leapYears;
