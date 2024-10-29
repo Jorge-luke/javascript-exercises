f3b2e93eb5d57954151766569614a79d3ab7a08e
jorge-luke <jorgestl123@gmail.com>
2024-10-29 01:22:57 -0300

    Done with execise 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(words,num) {
 
-};
+    if (num < 0) return "ERROR";
+
+   let string = "";
+    for(let i=0; i<num;i++){
+        string += words;
+        }
+
+        return string;
+
+    }
+
+
+
+    
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
+let arr = [1,2,3,4,5];
+
+const removeFromArray = function(array, ...items) {
+
+
+    for (let i=array.length-1;i>=0;i--){
+         
+   if (items.includes(array[i])){
+        array.splice(i,1);
+}
+
+}
+    return array;
+}
+
+console.log(arr);
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
